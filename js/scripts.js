a39b67e700897b8a87daa8d41854523824a98675
nsncowan <nsncowan@gmail.com>
2022-08-27 10:37:12 -0700

    add JS code establishing variable and conditionals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
 // function hide results and error
+function hideResults() {
+  document.getElementById("result1").setAttribute("class", "hidden");
+  document.getElementById("result2").setAttribute("class", "hidden");
+  document.getElementById("result3").setAttribute("class", "hidden");
+}
 
 // function to clear previous results
+    //document.getElementById('userName').reset();
+    //document.getElementById('languageSurvey').reset();
 
 // set variables containing input values for each form
+const name = document.getElementById("userNameInput").value;
+const priority = document.getElementById("priority").value;
+
+// conditionals to display results
+function processSurvey(event) {
+  event.preventDefault();
+  document.querySelector("span#name").innerText = userNameInput;
+  if (priority === "1") {
+    document.getElementById("result1").removeAttribute("class");
+  } else if (priority === "2") {
+    document.getElementById("result2").removeAttribute("class");
+  } else if (priority === "3") {
+    document.getElementById("result3").removeAttribute("class");
+  }
+}
+
+//Execute process
+
+
