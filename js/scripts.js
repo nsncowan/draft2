9b227bffdd842da22c527b947fc28c38cabf0a3c
nsncowan <nsncowan@gmail.com>
2022-08-27 15:48:34 -0700

    add additional conditionals and html.

--- js/scripts.js
+++ js/scripts.js
@@ -19,18 +19,28 @@ function processSurvey(event) {
   event.preventDefault();
   hideResults();
   const priority = document.getElementById("priority").value;
+  const help = document.querySelector("input[name='revolution']:checked").value
   let name = document.getElementById("userNameInput").value;
   
-  if (priority === "1") {
+  if (priority === "1" && help === "yes") {
     document.getElementById("result1").removeAttribute("class");
     document.querySelector("span#name1").innerText = name;
-  } else if (priority === "2") {
+  } else if (priority === "2" && help === "yes") {
     document.getElementById("result2").removeAttribute("class");
     document.querySelector("span#name2").innerText = name;
-  } else if (priority === "3") {
+  } else if (priority === "3" && help === "yes") {
     document.getElementById("result3").removeAttribute("class");
     document.querySelector("span#name3").innerText = name;
     document.querySelector("span#name4").innerText = name;
+  } else if (priority === "1" && help === "no") {
+    document.getElementById("result4").removeAttribute("class");
+    document.querySelector("span#name5").innerText = name;
+  } else if (priority === "2" && help === "no") {
+    document.getElementById("result5").removeAttribute("class");
+    document.querySelector("span#name6").innerText = name;
+  } else if (priority === "3" && help === "no") {
+    document.getElementById("result6").removeAttribute("class");
+    document.querySelector("span#name7").innerText = name;
   }
 }
 
